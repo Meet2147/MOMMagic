09bad30bf976c9f8edd80e7511b2ada8bc249c63
Meet Jethwa <meetjethwa3@gmail.com>
2024-07-09 01:19:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -86,6 +86,3 @@ async def process_audio(file: UploadFile = File(...)):
         tasks=tasks
     )
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
